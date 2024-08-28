073833046cb8dfb0875616228b9c3da7605edd36
gatewooda <austing767@icloud.com>
2024-08-28 11:54:52 -0400

    removed todo

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
     Original code and ASCII art by Dr. Jan Pearce, Berea College
 
     Edits made by:
-    TODO: Austin
-    TODO: <Add name of student 2 Here> -- to be added by student 2
-    TODO: <Add name of student 3 Here> -- to be added by student 3 (if relevant otherwise, student 2 should remove this line)
+    Austin
+    <Add name of student 2 Here> -- to be added by student 2
+    <Add name of student 3 Here> -- to be added by student 3 (if relevant otherwise, student 2 should remove this line)
  */
 #include <iostream>
 using namespace std;
